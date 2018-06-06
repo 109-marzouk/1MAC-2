2c4a456538422744b1e67520f403cdf4e62c5edf
unknown <alhalis60@gmail.com>
2018-06-07 00:32:18 +0200

    First Commit :)

---
+++ index.py
@@ -0,0 +1,16 @@
+"""
+https://macdiscussions.udacity.com/t/git-github/205621/
+====================
+"""
+
+quotes = [
+    "“Any fool can write code that a computer can understand. Good programmers write code that humans can understand.” – Martin Fowler",
+    "“First, solve the problem. Then, write the code.” – John Johnson",
+    "“Experience is the name everyone gives to their mistakes.” – Oscar Wilde",
+    "“ In order to be irreplaceable, one must always be different” – Coco Chanel",
+    "“Java is to JavaScript what car is to Carpet.” – Chris Heilmann"
+]
+from random import choice
+print(choice(quotes)
+
+#-------------------------------------------------------------------------------------
